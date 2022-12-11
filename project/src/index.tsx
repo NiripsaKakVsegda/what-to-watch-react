1cc738da0607a2eb8caa760bf2b2f3f821b6370b
NiripsaKakVsegda <anna.poluyanenko@gmail.com>
2022-12-11 08:58:55 +0500

    easy

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import ErrorMessage from './components/error-message/error-message';
+import { ToastContainer } from 'react-toastify';
 import { checkAuthAction, fetchMovieAction } from './store/api-actions';
+import 'react-toastify/dist/ReactToastify.css';
 
 store.dispatch(fetchMovieAction());
 store.dispatch(checkAuthAction());
@@ -16,7 +17,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store = {store}>
-      <ErrorMessage />
+      <ToastContainer />
       <App myMovies={[]}/>
     </Provider>
   </React.StrictMode>,
--- project/src/pages/sign-in-page/sign-in-page.tsx
+++ project/src/pages/sign-in-page/sign-in-page.tsx
@@ -1,8 +1,12 @@
-import { FC } from 'react';
+import { FC, FormEvent, useRef } from 'react';
 import Logo from '../../components/logo/logo';
 import Footer from '../../components/footer/footer';
 import SignInMessage from '../../components/sign-in-message/sign-in-message';
 import { SignInType } from '../../types/sign-in-type.enum';
+import { useAppDispatch } from '../../hooks';
+import { useNavigate } from 'react-router-dom';
+import { AuthData } from '../../types/auth-data';
+import { loginAction } from '../../store/api-actions';
 
 type Props = {
   signInType?: SignInType;
@@ -12,6 +16,27 @@ const SignInPage: FC<Props> = (props) => {
   let { signInType } = props;
   signInType = signInType ?? SignInType.Regular;
 
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const onSubmit = (authData: AuthData) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      onSubmit({
+        login: loginRef.current.value,
+        password: passwordRef.current.value,
+      });
+    }
+  };
+
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
@@ -19,18 +44,18 @@ const SignInPage: FC<Props> = (props) => {
         <h1 className="page-title user-page__title">Sign in</h1>
       </header>
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
+        <form action="#" className="sign-in__form" onSubmit={handleSubmit}>
           {signInType === SignInType.Error ?
             <SignInMessage message={'Please enter a valid email address'}/> : null}
           {signInType === SignInType.Message ?
             <SignInMessage message={'We can’t recognize this email <br/> and password combination. Please try again.'}/> : null}
           <div className="sign-in__fields">
             <div className={`sign-in__field${signInType === SignInType.Error ? ' sign-in__field--error' : ''}`}>
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email"/>
+              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" ref={loginRef}/>
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password"/>
+              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" ref={passwordRef}/>
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
